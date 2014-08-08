9eb6c115c3896bec041bc8937c2964f24f126e8f
vizvamitra <vizvamitra@gmail.com>
2014-08-08 20:13:01 +0400

    registration step definitions

---
+++ features/step_definitions/user_steps.rb
@@ -0,0 +1,11 @@
+Given(/^the user "([^"]*)" eixsts$/) do |name|
+  User.create(login: name, password: "password", password_confirmation: "password")
+end
+
+When(/^(?:|I )try to register with "([^"]*)", "([^"]*)"(?:, "([^"]*)")?$/) do |name, pass, confirm|
+  step 'I press "start_registration"'
+  step %Q{I fill in "login" with "#{name}"}
+  step %Q{I fill in "password" with "#{pass}"}
+  step %Q{I fill in "password_confirmation" with "#{confirm || pass}"}
+  step 'I press "register"'
+end
